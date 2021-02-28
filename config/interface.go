53f5e838b517d9edad0181faee98bbdf7586fd32
Peter Wood <peter@alastria.net>
2021-02-28 11:02:19 +0000

    Add configuration for MQTT.

--- config/interface.go
+++ config/interface.go
@@ -21,18 +21,42 @@ func (g *InterfaceConfig) UnmarshalJSON(data []byte) error {
 
 	switch g.Type {
 	case "http":
-		if result := gjson.GetBytes(data, "Config"); result.Exists() {
-			g.Config = &HTTPInterfaceConfig{}
-			return json.Unmarshal([]byte(result.Raw), g.Config)
-		} else {
-			return fmt.Errorf("unable to find Config stanza: %s", g.Type)
-		}
+		g.Config = &HTTPInterfaceConfig{}
+	case "mqtt":
+		g.Config = &MQTTInterfaceConfig{}
 	default:
 		return fmt.Errorf("unknown interface configuration type: %s", g.Type)
+	}
+
+	if result := gjson.GetBytes(data, "Config"); result.Exists() {
+		return json.Unmarshal([]byte(result.Raw), g.Config)
+	} else {
+		return fmt.Errorf("unable to find Config stanza: %s", g.Type)
 	}
 }
 
 type HTTPInterfaceConfig struct {
 	Port        int
 	EnabledAPIs []string
+}
+
+type MQTTInterfaceConfig struct {
+	Server string
+
+	TLS         MQTTTLS
+	Credentials MQTTCredentials
+
+	Retained bool
+	QOS      byte
+}
+
+type MQTTTLS struct {
+	Key    string
+	Cert   string
+	CACert string
+}
+
+type MQTTCredentials struct {
+	Username string
+	Password string
 }
