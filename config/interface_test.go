c055304c9d308e2bcbfd18b288460abfbef756b8
Peter Wood <peter@alastria.net>
2021-02-28 11:02:19 +0000

    Add configuration for MQTT.

--- config/interface_test.go
+++ config/interface_test.go
@@ -25,7 +25,15 @@ func TestParseInterface(t *testing.T) {
 
 	t.Run("http gateway", func(t *testing.T) {
 		t.Run("parses successfully", func(t *testing.T) {
-			data := []byte(`{"Type":"http","Config":{"Port":3000,"EnabledAPIs":["v1"]}}`)
+			data := []byte(`{
+  "Type": "http",
+  "Config": {
+    "Port": 3000,
+    "EnabledAPIs": [
+      "v1"
+    ]
+  }
+}`)
 			gw := InterfaceConfig{}
 
 			err := json.Unmarshal(data, &gw)
@@ -36,6 +44,47 @@ func TestParseInterface(t *testing.T) {
 
 			assert.Equal(t, 3000, httpInt.Port)
 			assert.Contains(t, httpInt.EnabledAPIs, "v1")
+		})
+	})
+
+	t.Run("mqtt gateway", func(t *testing.T) {
+		t.Run("parses successfully", func(t *testing.T) {
+			data := []byte(`{
+  "Type": "mqtt",
+  "Config": {
+    "Server": "tcp://127.0.0.1:1883",
+    "TLS": {
+      "Key": "key.pem",
+      "Cert": "cert.pem",
+      "CACert": "cacert.pem"
+    },
+    "Credentials": {
+      "Username": "user",
+      "Password": "pass"
+    },
+    "Retained": true,
+    "QOS": 2
+  }
+}`)
+			gw := InterfaceConfig{}
+
+			err := json.Unmarshal(data, &gw)
+			assert.NoError(t, err)
+
+			mqttInt, ok := gw.Config.(*MQTTInterfaceConfig)
+			assert.True(t, ok)
+
+			assert.Equal(t, "tcp://127.0.0.1:1883", mqttInt.Server)
+
+			assert.Equal(t, "key.pem", mqttInt.TLS.Key)
+			assert.Equal(t, "cert.pem", mqttInt.TLS.Cert)
+			assert.Equal(t, "cacert.pem", mqttInt.TLS.CACert)
+
+			assert.Equal(t, "user", mqttInt.Credentials.Username)
+			assert.Equal(t, "pass", mqttInt.Credentials.Password)
+
+			assert.True(t, mqttInt.Retained)
+			assert.Equal(t, uint8(2), mqttInt.QOS)
 		})
 	})
 }
