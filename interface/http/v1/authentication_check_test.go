b2eebd891c3c23a53ebd696eb73298314667991a
Peter Wood <peter@alastria.net>
2021-05-04 20:50:50 +0100

    Add authentication middleware to api/v1 paths where appropriate.

---
+++ interface/http/v1/authentication_check_test.go
@@ -0,0 +1,41 @@
+package v1
+
+import (
+	"context"
+	"github.com/shimmeringbee/controller/interface/http/auth"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_authenticationCheck(t *testing.T) {
+	t.Run("returns a not authenticated value if there is no user identity", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/auth/check", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+
+		authenticationCheck(rr, req)
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Equal(t, "{\"authenticated\":false}", rr.Body.String())
+	})
+
+	t.Run("returns a authenticated value if there is a user identity", func(t *testing.T) {
+		req, err := http.NewRequest("GET", "/auth/check", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req = req.WithContext(context.WithValue(req.Context(), auth.UserIdentityContextKey, "username"))
+		rr := httptest.NewRecorder()
+
+		authenticationCheck(rr, req)
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Equal(t, "{\"authenticated\":true,\"identity\":\"username\"}", rr.Body.String())
+	})
+}
