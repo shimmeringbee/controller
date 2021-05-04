15dc08f0765f07c86202706075b28485934820fe
Peter Wood <peter@alastria.net>
2021-05-04 22:03:39 +0100

    Add external authentication handler, used for authentication by an external webserver.
    The webserver may forward the username by providing it on the specified header, this may be the standard HTTP_USER or may be custom if for example an email address from MTLS authentication.

---
+++ interface/http/auth/external/handler.go
@@ -0,0 +1,38 @@
+package external
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/shimmeringbee/controller/interface/http/auth"
+	"net/http"
+)
+
+var _ auth.AuthenticationProvider = (*Authenticator)(nil)
+
+type Authenticator struct {
+	UserHeader string
+}
+
+const HttpUserHeader string = "HTTP_USER"
+
+func (a Authenticator) AuthenticationMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := r.Header.Get(a.UserHeader)
+		if len(user) == 0 {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), auth.UserIdentityContextKey, user)))
+	})
+}
+
+func (a Authenticator) AuthenticationRouter() http.Handler {
+	return mux.NewRouter()
+}
+
+func (a Authenticator) AuthenticationType() interface{} {
+	return auth.AuthenticatorType{
+		Type: "external",
+	}
+}
